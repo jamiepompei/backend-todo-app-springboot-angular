1d2d096411dba5379cdea3a76ef201ce9e715970
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-01 19:26:18 -0500

    added findbyusername method to todoJparepo and refactored method bodies of getToDo and getAllTodos

--- src/main/java/com/in28minutes/webservices/restfulwebservices/controllers/TodoControllerJpa.java
+++ src/main/java/com/in28minutes/webservices/restfulwebservices/controllers/TodoControllerJpa.java
@@ -1,5 +1,6 @@
 package com.in28minutes.webservices.restfulwebservices.controllers;
 import com.in28minutes.webservices.restfulwebservices.entities.Todo;
+import com.in28minutes.webservices.restfulwebservices.repositories.TodoJpaRepository;
 import com.in28minutes.webservices.restfulwebservices.services.TodoHardCodedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,14 +18,19 @@ public class TodoControllerJpa {
     @Autowired
     private TodoHardCodedService todoService;
 
+    @Autowired
+    private TodoJpaRepository todoJpaRepository;
+
     @GetMapping("/jpa/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username){
-        return todoService.findAll();
+        return todoJpaRepository.findByUsername(username);
+       // return todoService.findAll();
     }
 
     @GetMapping("/jpa/users/{username}/todos/{id}")
     public Todo getTodo(@PathVariable String username, @PathVariable Long id){
-        return todoService.findById(id);
+        return todoJpaRepository.findById(id).get();
+        //return todoService.findById(id);
     }
 
     @DeleteMapping("/jpa/users/{username}/todos/{id}")
--- src/main/java/com/in28minutes/webservices/restfulwebservices/repositories/TodoJpaRepository.java
+++ src/main/java/com/in28minutes/webservices/restfulwebservices/repositories/TodoJpaRepository.java
@@ -1,4 +1,13 @@
 package com.in28minutes.webservices.restfulwebservices.repositories;
 
-public class TodoJpaRepository {
+import com.in28minutes.webservices.restfulwebservices.entities.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoJpaRepository extends JpaRepository<Todo, Long> {
+
+ List<Todo> findByUsername(String username);
 }
